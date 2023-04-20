5e732a30b350ec06c139ca21d9c1a399a5aedbae
Slava Kholopov <slava.kholopov@gmail.com>
2023-04-20 09:21:38 +0200

    Добавил проверку, что кол-во сим карт больше нуля в инициалиацию класса, а также в сеттер

--- src/phone.py
+++ src/phone.py
@@ -3,8 +3,17 @@ from src.item import Item
 class Phone(Item):
     def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
-
-
+        if number_of_sim <= 0:
+            raise ValueError("ValueError: Количество физических SIM-карт должно быть целым числом больше нуля.")
+        else:
+            self.number_of_sim = number_of_sim
+    @property
+    def number_of_sim(self):
+        return self._number_of_sim
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        if value <= 0:
+            raise ValueError("ValueError: Количество физических SIM-карт должно быть целым числом больше нуля.")
+        self._number_of_sim = value
     def __repr__(self):
         return f"Phone('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
